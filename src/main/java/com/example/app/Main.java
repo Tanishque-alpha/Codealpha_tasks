ad779df8c362f12e1db1d28ecca7bedc911223cc
Tanishque Shrivastava <www.tanishquest@gmail.com>
2025-12-30 02:05:57 +0530

    Initial Java application using Gradle

---
+++ src/main/java/com/example/app/Main.java
@@ -0,0 +1,8 @@
+package com.example.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Application using Gradle");
+        System.out.println("BUILD SUCCESSFUL");
+    }
+}
