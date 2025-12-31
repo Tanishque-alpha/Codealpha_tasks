0570e3e29500f7c02cfbb5ee03272dde846a9164
Tanishque-alpha <www.tanishquest@gmail.com>
2025-12-31 21:13:43 +0530

    Delete src directory

--- src/main/java/com/example/app/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.app;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Java Application using Gradle");
-        System.out.println("BUILD SUCCESSFUL");
-    }
-}
